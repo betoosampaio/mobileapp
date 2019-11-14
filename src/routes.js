134f0cdd286299a220731c7ab237186ce729b86d
DiegoHonorio <honorioproduction@gmail.com>
2019-11-14 17:22:14 -0300

    Adicionado APP MOBILE Primeiro commit

---
+++ src/routes.js
@@ -0,0 +1,19 @@
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+
+
+
+
+import Main from '~/pages/Main';
+
+
+
+
+
+
+const Routes = createAppContainer(createSwitchNavigator({ Main }));
+
+
+
+
+
+export default Routes;
