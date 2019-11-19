d4195e657566aefebeca19f94a5f3e613e0509c2
DiegoHonorio <honorioproduction@gmail.com>
2019-11-19 14:40:49 -0300

    Adicionado Tela de cadastro

--- src/views/registrar/registrar.js
+++ src/views/registrar/registrar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from '../../assets/img/logo.png'
 import facebook from '../../assets/img/icons/facebook.png'
 import google from '../../assets/img/icons/google.png'
+import TextInputMask from 'react-native-text-input-mask';
 
 import {
   Text, Image, StyleSheet, Dimensions, ImageBackground, StatusBar, TouchableOpacity, Alert, View
@@ -13,50 +14,26 @@ const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
     flex: 1,
-    paddingHorizontal: 20,
   },
-  fileName: {
+  cadastrar: {
+    marginTop: 50,
+    color: '#fff',
+    fontSize: 22,
     fontWeight: 'bold',
-    marginTop: 5,
+    marginBottom: 50,
   },
-  instructions: {
-    color: '#DDD',
-    fontSize: 14,
-    marginTop: 20,
-    textAlign: 'center',
-  },
-  logo: {
-    height: Dimensions.get('window').height * 0.11,
-    marginVertical: Dimensions.get('window').height * 0.11,
-    width: Dimensions.get('window').height * 0.11 * (1950 / 662),
-  },
-  iconFace: {
-    height: 50,
-    width: 50,
+  input: {
+    marginTop: 10,
+    borderRadius: 5,
+    backgroundColor: '#FFF',
+    marginBottom: 10,
+    width: 330
   },
   informacao: {
     color: '#fff',
     fontSize: 22,
     fontWeight: 'bold',
-    marginRight: 230,
-  },
-  input: {
-    borderRadius: 5,
-    backgroundColor: '#FFF',
-    marginBottom: 10,
-    width: 300
-  },
-  buttonStyle: {
-    marginTop: 200,
-  },
-  cadastrar: {
-    color: '#fff',
-    textDecorationLine: 'underline',
-  },
-  containerImgLogin: {
-    marginTop: 50,
-    flexDirection: 'row',
-    marginBottom: 40
+    marginRight: 260,
   },
   button:{
     marginTop:20,
@@ -72,31 +49,57 @@ const styles = StyleSheet.create({
     fontWeight:'bold',
     fontSize:16,
   },
-  cadastrar1:{
-    color: '#000',
-    fontWeight:'bold',
-    fontSize:16,
-  }
-
 });
 
 const Registrar = () => (
   <ImageBackground
     source={{
-      uri: 'https://s3-sa-east-1.amazonaws.com/rocketseat-cdn/background.png',
+      uri: 'https://www.colorhexa.com/7159c1.png',
     }}
     style={styles.container}
     resizeMode="cover"
   >
     <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-    <Image
-      source={logo}
-      style={styles.logo}
-      resizeMode="contain"
+
+    <Text style={styles.cadastrar}>Cadastrar</Text>
+
+    <Text style={styles.informacao}>Nome:</Text>
+    <TextInput
+      style={styles.input}
+      placeholder="Qual é o seu nome ?"
+    />
+    <Text style={styles.informacao}>CPffff:</Text>
+    <TextInputMask
+      style={styles.input}
+      placeholder="Qual é o seu CPF ?"
+      keyboardType={'numeric'}
+      mask={"[000].[000].[000]-[00]"}
+    />
+    <Text style={styles.informacao}>Telefone:</Text>
+    <TextInputMask
+      style={styles.input}
+      placeholder="E o seu Telefone?"
+      keyboardType={'numeric'}
+      mask={"([00]) [00000]-[0000]"}
+    />
+    <Text style={styles.informacao}>E-mail:</Text>
+    <TextInput
+      style={styles.input}
+      placeholder="Insira o Seu E-mail"
+    />
+    <Text style={styles.informacao}>Senha:</Text>
+    <TextInput
+      style={styles.input}
+      placeholder="Insira o Sua Senha"
     />
 
 
-    
+
+    <TouchableOpacity style={styles.button}>
+      <Text style={styles.buttonText}>Registrar</Text>
+    </TouchableOpacity>
+
+
 
   </ImageBackground>
 );
